5ba9d8d678a82cb755d8458ec77366d48a8ed43c
davis8988 <Davismarsel@gmail.com>
2021-10-05 22:52:43 +0300

    Printings

--- src/davis8988_hello/shell.py
+++ src/davis8988_hello/shell.py
@@ -7,7 +7,7 @@ def execute(**kwargs):
     command_str = kwargs.get("command_str")  # Returns None if key not found
     command_timeout = kwargs.get("command_timeout")  
     if command_str is None:
-        
+        summary_dict['info'] = "Missing mandatory param for function execute() : 'command_str' "
         return summary_dict
     
     
