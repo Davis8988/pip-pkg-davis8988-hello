873bc4704347091e88dfb607cf24cf179894872a
davis8988 <Davismarsel@gmail.com>
2021-10-05 22:50:02 +0300

    Added var:   result_dict = {"status" : False, "info" : ''}

--- src/davis8988_hello/shell.py
+++ src/davis8988_hello/shell.py
@@ -3,6 +3,7 @@
 import subprocess
 
 def execute(**kwargs):
+    result_dict = {"status" : False, "info" : ''}
     command_str = kwargs.get("command_str")  # Returns None if key not found
     command_timeout = kwargs.get("command_timeout")  
     if command_str is None:
