73bd68262945a5051aba06b62df7713678d795ec
davis8988 <Davismarsel@gmail.com>
2021-10-05 22:42:16 +0300

    Added def execute(**kwargs): func

--- src/davis8988_hello/shell.py
+++ src/davis8988_hello/shell.py
@@ -1,6 +1,10 @@
 # Executes a command on windows CMD or unix Shell 
 
 import subprocess
+
+def execute(**kwargs):
+    pass
+
 result = subprocess.run("dir /b", shell=True)
 
 print("Result:", result)
