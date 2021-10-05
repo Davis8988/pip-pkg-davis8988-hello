3f053b1a9b54dd2edf5e9582735a1f6434b1c06e
davis8988 <Davismarsel@gmail.com>
2021-10-05 22:50:59 +0300

    Renamed to: summary_dict

--- src/davis8988_hello/shell.py
+++ src/davis8988_hello/shell.py
@@ -3,12 +3,13 @@
 import subprocess
 
 def execute(**kwargs):
-    result_dict = {"status" : False, "info" : ''}
+    summary_dict = {"status" : False, "info" : ''}
     command_str = kwargs.get("command_str")  # Returns None if key not found
     command_timeout = kwargs.get("command_timeout")  
     if command_str is None:
         print("Error - Missing mandatory param for function execute() : 'command_str' ")
-        return result_dict
+        return summary_dict
+    
     
     result = subprocess.run("dir /b", shell=True)
     print("Result:", result)    
