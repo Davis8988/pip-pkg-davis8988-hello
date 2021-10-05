478522c874865223aea1bc69644ff1f9bc789ab4
davis8988 <Davismarsel@gmail.com>
2021-10-05 22:48:10 +0300

    Added check for command_str

--- src/davis8988_hello/shell.py
+++ src/davis8988_hello/shell.py
@@ -5,6 +5,10 @@ import subprocess
 def execute(**kwargs):
     command_str = kwargs.get("command_str")  # Returns None if key not found
     command_timeout = kwargs.get("command_timeout")  
+    if command_str is None:
+        print("Error - Missing mandatory param for function execute() : 'command_str' ")
+        return None
+    
     result = subprocess.run("dir /b", shell=True)
     print("Result:", result)    
 
