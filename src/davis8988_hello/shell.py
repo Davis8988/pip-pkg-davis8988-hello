4d52f178d9ebe22889a31dc41599042080192a87
davis8988 <Davismarsel@gmail.com>
2021-10-30 23:59:04 +0300

    Re-raising TimeoutError obkj

--- src/davis8988_hello/shell.py
+++ src/davis8988_hello/shell.py
@@ -61,8 +61,9 @@ def execute(command_str, **kwargs):
                     proc_out += line + "\n"
                     printing_func("Stderr:", line)
             time.sleep(1)
-    except TimeoutError:
+    except TimeoutError as err_msg:
         process_obj.kill()
+        raise TimeoutError(err_msg)
     finally:
         timer.cancel()
     
