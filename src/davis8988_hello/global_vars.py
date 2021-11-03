d40bba0635342daaf42b61ac429d17ac6cbbd014
davis8988 <Davismarsel@gmail.com>
2021-11-04 00:33:41 +0200

    Added 2 vars to: global_vars

--- src/davis8988_hello/global_vars.py
+++ src/davis8988_hello/global_vars.py
@@ -1,3 +1,6 @@
 # This module contains variables that should be used globally between modules
 
 
+# Logging
+_logging_format_date = '%d-%b-%y %H:%M:%S'
+_logging_format_console = '%(asctime)s - %(levelname)s : %(message)s'
