3f3703294708fcb621fde7b3e522832c4daa737a
davis8988 <Davismarsel@gmail.com>
2021-11-04 00:20:37 +0200

    Using func: get_root_logger_or_fail()

--- src/davis8988_hello/shell.py
+++ src/davis8988_hello/shell.py
@@ -7,11 +7,7 @@ from threading import Timer
 
 
 # Get root logger
-get_root_logger_result_dict = root_logging.get_root_logger() 
-if not get_root_logger_result_dict['result']:
-    print(get_root_logger_result_dict['info'])  # This should occur on import failures - then printing & raising exceptions are required
-    raise Exception(get_root_logger_result_dict['info']) 
-root_logger = get_root_logger_result_dict['root_logger']
+root_logger = root_logging.get_root_logger_or_fail()
 
 
 def _command_skip_printings(msg):
