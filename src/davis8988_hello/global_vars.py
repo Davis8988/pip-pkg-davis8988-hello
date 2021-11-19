85a128c6a646ca30009cef99704162576a8fe312
davis8988 <Davismarsel@gmail.com>
2021-11-19 17:17:27 +0200

    Printings

--- src/davis8988_hello/global_vars.py
+++ src/davis8988_hello/global_vars.py
@@ -6,6 +6,6 @@ from os import environ as env_vars_dict
 # Logging
 _logging_format_date = '%H:%M:%S'
 _logging_console_level = env_vars_dict.get('LOGLEVEL', 'DEBUG').upper()
-
-_logging_console_format = '[%(levelname)-5s] %(asctime)s : %(message)s'
+spaces_count = len(_logging_console_level)
+_logging_console_format = f'[%(levelname)-{spaces_count}s] %(asctime)s : %(message)s'
 _logging_console_date_format = _logging_format_date
