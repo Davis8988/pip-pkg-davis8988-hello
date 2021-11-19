3161bba0527b5bb82a9cf9583b47643343ad7f87
davis8988 <Davismarsel@gmail.com>
2021-11-19 16:55:54 +0200

    Fixed global vars

--- src/davis8988_hello/global_vars.py
+++ src/davis8988_hello/global_vars.py
@@ -1,10 +1,10 @@
 # This module contains variables that should be used globally between modules
 
 import logging
-import os.environ
+from os import environ as env_vars_dict
 
 # Logging
 _logging_format_date = '%H:%M:%S'
 _logging_console_format = '[%(levelname)-5s] %(asctime)s : %(message)s'
 _logging_console_date_format = _logging_format_date
-_logging_console_level = os.environ.get('LOGLEVEL', 'DEBUG').upper()
+_logging_console_level = env_vars_dict.get('LOGLEVEL', 'DEBUG').upper()
