2601f65771a5ebda0a3fd537c0b064d6b8e47113
davis8988 <Davismarsel@gmail.com>
2021-11-19 16:49:58 +0200

    Updated logging level env var

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,7 +1,9 @@
 # Tests 'shell' module
 
 import davis8988_hello.shell as myshell
+import os
 
+os.environ['LOGLEVEL'] = 'info'
 result = myshell.execute("asdadsas ping localhost -n 2", command_timeout_sec=5)
 myshell.execute("ping localhost -n 10")
 
